54fcea53368ca417f6e97748061028d45974c7ed
nanguazhude <nanguazhude@vip.qq.com>
2017-11-28 07:57:38 +0800

    +simpledrawling code

--- SimpleDrawLine.cpp
+++ SimpleDrawLine.cpp
@@ -11,7 +11,21 @@ namespace sstd {
 	}
 
 	void SimpleDrawLine::main() {
+		const AcGePoint3d varStart{0,0,0};
+		const AcGePoint3d varEnd{100,100,0};
+		sstd::ArxClosePointer<AcDbLine> varLine{ new AcDbLine{varStart,varEnd} };
 
+		sstd::ArxClosePointer<AcDbBlockTable> varBlockTable;
+		sstd::ArxClosePointer<AcDbBlockTableRecord> varBlockTableRecord;
+
+		acdbHostApplicationServices()->workingDatabase()
+			->getBlockTable(varBlockTable,AcDb::kForRead);
+		varBlockTable->getAt(ACDB_MODEL_SPACE, varBlockTableRecord, AcDb::kForWrite);
+
+		if (varBlockTable&&varBlockTableRecord) {
+			AcDbObjectId varLineID;
+			varBlockTableRecord->appendAcDbEntity(varLineID,varLine);
+		}
 	}
 
 }/*namespace sstd*/
