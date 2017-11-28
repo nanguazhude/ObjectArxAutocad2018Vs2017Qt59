2c057dccec952a503438f357d9635f9b5b2b0fe0
nanguazhude <nanguazhude@vip.qq.com>
2017-11-28 10:32:54 +0800

    +simple draw line change color

--- SimpleDrawLineChangeColor.cpp
+++ SimpleDrawLineChangeColor.cpp
@@ -2,6 +2,8 @@
 
 namespace sstd {
 
+	extern void loadSimpleDrawLineChangeColor() { SimpleDrawLineChangeColor::load(); }
+
 	SimpleDrawLineChangeColor::SimpleDrawLineChangeColor() {
 
 	}
@@ -10,8 +12,37 @@ namespace sstd {
 		arx_add_main_command<SimpleDrawLineChangeColor>();
 	}
 
-	void SimpleDrawLineChangeColor::main() {
+	inline AcDbObjectId simpleDrawLine() {
+		const AcGePoint3d varStart{ 0,0,0 };
+		const AcGePoint3d varEnd{ 100,100,0 };
+		sstd::ArxClosePointer<AcDbLine> varLine{ new AcDbLine{ varStart,varEnd } };
 
+		sstd::ArxClosePointer<AcDbBlockTable> varBlockTable;
+		sstd::ArxClosePointer<AcDbBlockTableRecord> varBlockTableRecord;
+
+		acdbHostApplicationServices()->workingDatabase()
+			->getBlockTable(varBlockTable, AcDb::kForRead);
+		if (varBlockTable) {
+			varBlockTable->getAt(ACDB_MODEL_SPACE, varBlockTableRecord,
+				AcDb::kForWrite);
+			if (varBlockTableRecord) {
+				AcDbObjectId varLineID;
+				varBlockTableRecord->appendAcDbEntity(varLineID, varLine);
+				return varLineID;
+			}
+		}
+		return{};
+	}
+
+	void SimpleDrawLineChangeColor::main() {
+		AcDbObjectId varLineID = simpleDrawLine();
+		if (varLineID.isNull()) { return; }
+		constexpr const Adesk::UInt16 varColorIndex = 1;
+		sstd::ArxClosePointer<AcDbEntity> varEntity;
+		sstd::openObject(varEntity, varLineID, AcDb::kForWrite);
+		if (varEntity) {
+			varEntity->setColorIndex(varColorIndex);
+		}
 	}
 
 }/*namespace sstd*/
