4281aeaf24c34dadc8b721ada8999bcc947663b3
nanguazhude <nanguazhude@vip.qq.com>
2017-12-02 11:39:31 +0800

    +Test Code

--- UpdateLayer/UpdateLayer.cpp
+++ UpdateLayer/UpdateLayer.cpp
@@ -20,7 +20,7 @@ namespace sstd {
 		inline ApplyMaps _p_createFunctions() {
 			ApplyMaps varAns;
 			/********************************************************/
-			varAns.emplace(LR"(0)"sv, ApplyMaps::value_type{ [](simple_code_args) {
+			varAns.emplace(LR"(0)"sv, ApplyMaps::value_type::second_type{ [](simple_code_args) {
 				sstd::ArxClosePointer<AcDbLayerTableRecord> varLocalLTR;
 				if (argLTR == nullptr) {
 					varLocalLTR = new AcDbLayerTableRecord;
@@ -29,19 +29,23 @@ namespace sstd {
 					argLT->add(argLTR);
 				}
 
+				/*初始化颜色*****************************************************/
 				AcCmColor varLColor;
 				varLColor.setColorIndex(211);
+				/*初始化线宽*****************************************************/
 				AcDb::LineWeight varLWeight = AcDb::kLnWt030;
+				/*设置透明度*****************************************************/
 				AcCmTransparency varLTP;
 				varLTP.setAlphaPercent(0);
+				/*初始化线型*****************************************************/
 				std::optional<AcDbObjectId> varLTypeID;
 				{
 					AcDbObjectId varLTypeIDTmp;
-					if (Acad::eOk == argLTT->getAt(LR"()", varLTypeIDTmp)) {
+					if (Acad::eOk == argLTT->getAt(LR"(Continuous)", varLTypeIDTmp)) {
 						*varLTypeID = varLTypeIDTmp;
 					}
 				}
-
+				/******************************************************/
 				argLTR->setIsOff(false)/*打开/关闭*/;
 				argLTR->setIsFrozen(false)/*冻结*/;
 				argLTR->setIsLocked(false)/*锁定*/;
@@ -62,11 +66,72 @@ namespace sstd {
 		inline void _p_update_layer( AcDbDatabase * argDB ) {
 			if (nullptr == argDB) { return; }
 			
+			/*获得线型表*/
+			sstd::ArxClosePointer< AcDbLinetypeTable > varLinetypeTable;
+			if ( Acad::eOk != argDB->getLinetypeTable(varLinetypeTable) ) {
+				acutPrintf(LR"(获得线型失败
+)");
+			}
+
+			/*获得图层表*/
+			sstd::ArxClosePointer< AcDbLayerTable > varLayerTable;
+			if ( Acad::eOk != argDB->getLayerTable(varLayerTable) ) {
+				acutPrintf(LR"(获得线型失败
+)");
+			}
+
+			/**/
+			auto varFunctions = _p_createFunctions();
+			const auto varFNoPos = varFunctions.end();
+
+			/*处理已经存在图层*/
+			{
+				std::unique_ptr<AcDbLayerTableIterator> varIt;
+				{
+					AcDbLayerTableIterator * varItTmp = nullptr;
+					if (Acad::eOk != varLayerTable->newIterator(varItTmp)) {
+						return ;
+					}
+					varIt.reset(varItTmp);
+				}
+
+				for (varIt->start(); !varIt->done(); varIt->step()) {
+					sstd::ArxClosePointer< AcDbLayerTableRecord > varLTR;
+					if (Acad::eOk == varIt->getRecord(varLTR, AcDb::kForWrite )) {
+						wchar_t * varName = nullptr;
+						if (Acad::eOk == varLTR->getName(varName)) {
+							auto varCPos = varFunctions.find( varName );
+							if (varCPos == varFNoPos) { continue; }
+							varCPos->second.second = true;
+							varCPos->second.first(varCPos->first,
+								varLayerTable,
+								varLinetypeTable,
+								varLTR
+							);
+						}
+					}
+				}
+			}
+
+			/*创建新图层*/
+			for (auto & varI: varFunctions) {
+				if (varI.second.second==false) {
+					varI.second.second = true;
+					varI.second.first(varI.first, 
+						varLayerTable, 
+						varLinetypeTable,
+						nullptr);
+				}
+			}
+
 		}
 	}/*namespace*/
 
 	void UpdateLayer::main(){
-
+		_p_update_layer(acdbHostApplicationServices()->workingDatabase());
 	}
 
 }/*namespace sstd*/
+
+
+
