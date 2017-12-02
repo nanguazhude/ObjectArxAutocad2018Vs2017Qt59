69297b068f06c57e21b59f60630ba9c963a70e05
nanguazhude <nanguazhude@vip.qq.com>
2017-12-02 08:59:17 +0800

    +update layer

--- UpdateLayer/UpdateLayer.cpp
+++ UpdateLayer/UpdateLayer.cpp
@@ -1,4 +1,5 @@
 ﻿#include <map>
+#include <optional>
 #include "UpdateLayer.hpp"
 
 namespace sstd {
@@ -13,7 +14,7 @@ namespace sstd {
 	}
 
 	namespace {
-#define simple_code_args const std::wstring_view & argNM , AcDbLayerTable * argLT , AcDbLayerTableRecord * argLTR
+#define simple_code_args const std::wstring_view & argNM , AcDbLayerTable * argLT, AcDbLinetypeTable * argLTT , AcDbLayerTableRecord * argLTR
 		using ApplyLayerType = void(*)(simple_code_args);
 
 		inline std::map<std::wstring_view, ApplyLayerType> _p_createFunctions() {
@@ -29,24 +30,35 @@ namespace sstd {
 				}
 
 				AcCmColor varLColor;
-				AcDb::LineWeight varLWeight;
+				varLColor.setColorIndex(211);
+				AcDb::LineWeight varLWeight = AcDb::kLnWt030;
 				AcCmTransparency varLTP;
+				varLTP.setAlphaPercent(0);
+				std::optional<AcDbObjectId> varLTypeID;
+				{
+					AcDbObjectId varLTypeIDTmp;
+					if ( Acad::eOk == argLTT->getAt(LR"()", varLTypeIDTmp)) {
+						*varLTypeID = varLTypeIDTmp;
+					}
+				}
 
 				argLTR->setIsOff(false)/*打开/关闭*/;
 				argLTR->setIsFrozen(false)/*冻结*/;
 				argLTR->setIsLocked(false)/*锁定*/;
 				argLTR->setColor(varLColor)/*颜色*/;
-				/*设置线型*/;
-				argLTR->setLineWeight(varLWeight)/*线宽*/;
-				argLTR->setTransparency(varLTP)/*透明度*/;
 				argLTR->setIsPlottable(true)/*打印*/;
 				argLTR->setDescription(LR"(图层：0)")/*注释*/;
-
+				if (varLTypeID) { 
+					argLTR->setLinetypeObjectId(*varLTypeID)/*设置线型*/; 
+				}
+				argLTR->setLineWeight(varLWeight)/*线宽*/;
+				argLTR->setTransparency(varLTP)/*透明度*/;
+				
 			});
 			/********************************************************/
 			return std::move(varAns);
 		}
-
+		
 		inline void _p_update_layer( AcDbDatabase * argDB ) {
 			if (nullptr == argDB) { return; }
 			
