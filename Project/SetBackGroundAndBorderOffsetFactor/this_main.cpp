f234fd7937bbbe2435afe6c1a4987256f74c24c4
nanguazhude <nanguazhude@vip.qq.com>
2017-12-04 13:11:09 +0800

    -space

--- Project/SetBackGroundAndBorderOffsetFactor/this_main.cpp
+++ Project/SetBackGroundAndBorderOffsetFactor/this_main.cpp
@@ -5,64 +5,63 @@
 #pragma warning( pop)
 
 class EditorReactor :
-    public AcEditorReactor {
+	public AcEditorReactor {
 public:
-    inline void databaseConstructed(AcDbDatabase*) ;
+	inline void databaseConstructed(AcDbDatabase*);
 };
 
 class DatabaseReacotr :
-    public AcDbDatabaseReactor {
+	public AcDbDatabaseReactor {
 public:
-    inline void objectAppended(
-            const AcDbDatabase*  ,
-            const AcDbObject* dbObj) override {
-        auto varMtext = dynamic_cast<const AcDbMText*>(dbObj);
-        if (varMtext) {
-            double varBF = 0;
-            varMtext->getBackgroundScaleFactor(varBF);
-            if (std::abs(varBF - 1.005) < 0.0001) {
-                return;
-            }
+	inline void objectAppended(
+		const AcDbDatabase*,
+		const AcDbObject* dbObj) override {
+		auto varMtext = dynamic_cast<const AcDbMText*>(dbObj);
+		if (varMtext) {
+			double varBF = 0;
+			varMtext->getBackgroundScaleFactor(varBF);
+			if (std::abs(varBF - 1.005) < 0.0001) {
+				return;
+			}
 
-            auto var = const_cast<AcDbMText*>(varMtext);
-            if (Acad::eOk == var->upgradeOpen()) {
-                var->setBackgroundFill(true);
-                var->setUseBackgroundColor(true);
-                var->setBackgroundScaleFactor(1.005);
-            }
-        }
-    }
+			auto var = const_cast<AcDbMText*>(varMtext);
+			if (Acad::eOk == var->upgradeOpen()) {
+				var->setBackgroundFill(true);
+				var->setUseBackgroundColor(true);
+				var->setBackgroundScaleFactor(1.005);
+			}
+		}
+	}
 
 };
 
-DatabaseReacotr * globalDatabaseReacotr = 0 ;
-EditorReactor * globalEditorReactor = 0 ;
+DatabaseReacotr * globalDatabaseReacotr = 0;
+EditorReactor * globalEditorReactor = 0;
 
-void EditorReactor::databaseConstructed(AcDbDatabase* var)   {
-    var->addReactor(globalDatabaseReacotr);
+void EditorReactor::databaseConstructed(AcDbDatabase* var) {
+	var->addReactor(globalDatabaseReacotr);
 }
 
 extern "C" AcRx::AppRetCode
 acrxEntryPoint(AcRx::AppMsgCode msg, void* pkt) {
-    switch (msg) {
-    case AcRx::kInitAppMsg: {
-        acrxDynamicLinker->unlockApplication(pkt);
-        acrxRegisterAppMDIAware(pkt);
-        /*****************************************/
-        globalDatabaseReacotr = new DatabaseReacotr;
-        globalEditorReactor = new EditorReactor;
-        acedEditor->addReactor(globalEditorReactor);
-        /*****************************************/
-        break;
-    }
-    case AcRx::kUnloadAppMsg: {
-         
-        break;
-    }
-    default:break;
-    }
+	switch (msg) {
+	case AcRx::kInitAppMsg: {
+		acrxDynamicLinker->unlockApplication(pkt);
+		acrxRegisterAppMDIAware(pkt);
+		/*****************************************/
+		globalDatabaseReacotr = new DatabaseReacotr;
+		globalEditorReactor = new EditorReactor;
+		acedEditor->addReactor(globalEditorReactor);
+		/*****************************************/
+		break;
+	}
+	case AcRx::kUnloadAppMsg: {
+		break;
+	}
+	default:break;
+	}
 
-    return AcRx::kRetOK;
+	return AcRx::kRetOK;
 }
 
 /********************************/
