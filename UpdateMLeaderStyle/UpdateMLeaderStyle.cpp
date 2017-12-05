6678f6a1337ea3f0b74aa1003f9fa552fa2add64
nanguazhude <nanguazhude@vip.qq.com>
2017-12-05 22:02:33 +0800

    +Test Code

--- UpdateMLeaderStyle/UpdateMLeaderStyle.cpp
+++ UpdateMLeaderStyle/UpdateMLeaderStyle.cpp
@@ -1,4 +1,5 @@
 ﻿#include <map>
+#include <optional>
 #include <dbmleaderstyle.h>
 #include <object_arx_global.hpp>
 #include "UpdateMLeaderStyle.hpp"
@@ -6,41 +7,59 @@
 namespace sstd {
 
 	UpdateMLeaderStyle::UpdateMLeaderStyle() {
-
 	}
 
 	namespace {
 		class _UpdateMLeaderStyle {
 		public:
+			std::optional< AcDbObjectId > $TextTypeID;
 			AcDbDatabase * const $DB;
 			sstd::ArxClosePointer<AcDbDictionary> $MleaderTable;
-			using Function = void(*)(_UpdateMLeaderStyle* ,const std::wstring & , AcDbMLeaderStyle * );
+			using Function = void(*)(_UpdateMLeaderStyle*, const std::wstring &, AcDbMLeaderStyle *);
 			using Functions = std::map<std::wstring, std::pair<Function, bool>>;
 			Functions $Functions;
-			_UpdateMLeaderStyle(AcDbDatabase*arg) :$DB(arg){}
+			_UpdateMLeaderStyle(AcDbDatabase*arg) :$DB(arg) {}
 			inline void update();
 		private:
-			void _construct();
+			inline void _textTypeID();
+			inline void _construct();
 		};
+
+		inline void _UpdateMLeaderStyle::_textTypeID() {
+			AcDbTextStyleTable * varT;
+			if (kOk == $DB->getTextStyleTable(varT)) {
+				AcDbObjectId varID;
+				if (kOk == varT->getAt(LR"(@Standard)", varID)) {
+					$TextTypeID = varID;
+				}
+				varT->close();
+			}
+		}
 
 		inline void _UpdateMLeaderStyle::update() {
 			_construct();
 
 			if (kOk != $DB->getMLeaderStyleDictionary(
 				$MleaderTable.pointer(), AcDb::kForWrite)) {
+				acutPrintf(LR"(getMLeaderStyleDictionary
+)");
 				return;
 			}
+			else {
+				acutPrintf(LR"(getMLeaderStyleDictionary : ok
+)");
+			}
 
-			std::unique_ptr<AcDbDictionaryIterator> 
+			std::unique_ptr<AcDbDictionaryIterator>
 				varIt{ $MleaderTable->newIterator() };
 
 			if (varIt == nullptr) { return; }
 
 			const auto varNoPos = $Functions.end();
-			for (;!varIt->done();varIt->next()) {
+			for (; !varIt->done(); varIt->next()) {
 				sstd::ArxClosePointer<AcDbMLeaderStyle> varR;
 
-				if(kOk!= varIt->getObject(varR.pointer(), AcDb::kForWrite)){
+				if (kOk != varIt->getObject(varR.pointer(), AcDb::kForWrite)) {
 					continue;
 				}
 
@@ -48,7 +67,7 @@ namespace sstd {
 				{
 					wchar_t * varName = nullptr;
 					if (kOk != varR->getName(varName)) {
-						return;
+						continue;
 					}
 					varNameW = varName;
 				}
@@ -62,7 +81,7 @@ namespace sstd {
 
 			}
 
-			for (auto & varIt: $Functions) {
+			for (auto & varIt : $Functions) {
 				if (varIt.second.second) { continue; }
 				varIt.second.second = true;
 				varIt.second.first(this, varIt.first, nullptr);
@@ -70,13 +89,15 @@ namespace sstd {
 
 		}
 
-		void _UpdateMLeaderStyle::_construct() {
+		inline void _UpdateMLeaderStyle::_construct() {
+			if (false == bool($TextTypeID))_textTypeID();
 			$Functions.clear();
-			$Functions.emplace(LR"()"s, Functions::value_type::second_type{ [](_UpdateMLeaderStyle*argTable,
+			$Functions.emplace(LR"(Test)"s,
+				Functions::value_type::second_type{ [](_UpdateMLeaderStyle*argTable,
 				const std::wstring &argName,
 				AcDbMLeaderStyle *argR) {
 				sstd::ArxClosePointer<AcDbMLeaderStyle> varLocal;
-				if (argR==nullptr) {
+				if (argR == nullptr) {
 					AcDbObjectId varID;
 					argR = new AcDbMLeaderStyle;
 					varLocal = argR;
@@ -85,22 +106,41 @@ namespace sstd {
 						argR, varID);
 				}
 				/****************************************************************/
+				argR->setAnnotative(true)/*注释性*/;
+				argR->setArrowSize(4.6)/*箭头大小*/;
+				argR->setMaxLeaderSegmentsPoints(2);
+				argR->setBreakSize(0)/**/;
+				argR->setDescription(LR"(Hellow Kitty!)");
+				argR->setContentType(AcDbMLeaderStyle::kMTextContent);
+				if (argTable->$TextTypeID) argR->setTextStyleId(*argTable->$TextTypeID);
+				argR->setTextAngleType(AcDbMLeaderStyle::kHorizontalAngle);
+				argR->setTextAttachmentDirection(AcDbMLeaderStyle::kAttachmentHorizontal);
+				argR->setTextAttachmentType(AcDbMLeaderStyle::kAttachmentBottomLine, AcDbMLeaderStyle::kLeftLeader);
+				argR->setTextAttachmentType(AcDbMLeaderStyle::kAttachmentBottomLine, AcDbMLeaderStyle::kRightLeader);
+				argR->setExtendLeaderToText(true);
+				argR->setLandingGap(1.2);
+				argR->setEnableDogleg(false);
+				argR->setArrowSymbolId(LR"()");
 				/****************************************************************/
-			} ,false});
+			} ,false });
 		}
 
 	}/*namespace*/
 
-	extern void loadUpdateMLeaderStyle() { 
-		arx_add_main_command<UpdateMLeaderStyle>(); }
-
-	void UpdateMLeaderStyle::load(){
+	extern void loadUpdateMLeaderStyle() {
+		UpdateMLeaderStyle::load();
 	}
 
-	void UpdateMLeaderStyle::main(){
-		_UpdateMLeaderStyle varThisFunction( 
+	void UpdateMLeaderStyle::load() {
+		arx_add_main_command<UpdateMLeaderStyle>();
+	}
+
+	void UpdateMLeaderStyle::main() {
+		_UpdateMLeaderStyle varThisFunction(
 			acdbHostApplicationServices()->workingDatabase());
 		varThisFunction.update();
 	}
 
 }/*namespace sstd*/
+
+/**************************************************************/
