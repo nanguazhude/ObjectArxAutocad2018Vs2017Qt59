1d622ac7d586a16ebab99c3e8e44e7f3abfb8b20
nanguazhude <nanguazhude@vip.qq.com>
2017-12-06 16:47:22 +0800

    +sstd::get_c_style_string

--- StringUtility.hpp
+++ StringUtility.hpp
@@ -28,6 +28,40 @@ namespace sstd {
 #endif
 	using namespace std::string_literals;/*s*/
 	using namespace std::string_view_literals;/*sv*/
+
+	namespace ___private_string_u_sstd {
+		template<typename T, typename U = void>
+		class Has_C_Str_Function : public std::false_type {};
+
+		template<typename T>
+		class Has_C_Str_Function<T, std::void_t< decltype(
+			std::declval< std::add_const_t<
+			std::remove_reference_t<T> > >().c_str())>
+		> : public std::true_type {};
+
+
+		template<typename T>
+		inline const wchar_t * get_c_style_string(const T & arg) {
+			if constexpr(Has_C_Str_Function<T>::value) {
+				return arg.c_str();
+			}
+			else {
+				return arg.data();
+			}
+		}
+
+	}/*namespace ___private_string_u_sstd*/
+
+	template<typename T>
+	inline const wchar_t * get_c_style_string(const T & arg) {
+		if constexpr(std::is_pointer_v<std::remove_reference_t<T>>) {
+			return arg;
+		}
+		else {
+			return ___private_string_u_sstd::get_c_style_string(arg);
+		}
+	}
+
 }/*namespace sstd*/
 
 #endif // STRINGUTILITY_HPP
