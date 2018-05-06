474ba3a58e1f712c65f3612c7f92da1b624e7edc
nanguazhude <nanguazhude@vip.qq.com>
2018-05-06 10:22:57 +0800

    +Test Code

--- FEraseAll/FEraseAll.cpp
+++ FEraseAll/FEraseAll.cpp
@@ -7,11 +7,15 @@ namespace sstd {
 }/*namespace sstd*/
 
 void sstd::FEraseAll::load() {
-
+	arx_add_main_command<FEraseAll>();
 }
 
 void sstd::FEraseAll::main() {
-
+	//(command "erase" (ssget "x" (list (cons 0 "xline,ray" ) ) ) "" )
+	auto varCD = acDocManager->curDocument();
+	acDocManager->sendStringToExecute(
+		varCD, LR"......((command "erase" (ssget "x" (list (cons 0 "xline,ray" ) ) ) "" )
+)......");
 }
 
 
