a5f7a7808e97f589bc493fff77f6c7673407b9c7
nanguazhude <nanguazhude@vip.qq.com>
2018-05-15 20:53:35 +0800

    +Test Code

--- TestMultiFiles/TestMultiFiles.cpp
+++ TestMultiFiles/TestMultiFiles.cpp
@@ -6,7 +6,7 @@ namespace sstd {
 		acedRegCmds->addCommand(arx_group_name(),
 			T::globalName(),
 			T::localName(),
-			ACRX_CMD_MODAL,
+			ACRX_CMD_SESSION,
 			&T::main
 		);
 	}
@@ -17,6 +17,47 @@ void sstd::TestMultiFiles::load(){
 }
 
 static inline void _sstd_TestMultiFiles_main() {
+
+	class DocumentLock {
+	public:
+		DocumentLock() {
+			acDocManager->lockDocument(acDocManager->curDocument());
+		}
+		~DocumentLock() {
+			acDocManager->unlockDocument(acDocManager->curDocument());
+		}
+	};
+
+	//application  
+	
+	{//do somenthing int the Autocad opened documented
+		DocumentLock varDocumentLock;
+		auto varDataBase = acdbHostApplicationServices()->workingDatabase();
+		const wchar_t * varFileName;
+		varDataBase->getFilename(varFileName);
+		acutPrintf(LR"(CurrentDocument: %s
+)", varFileName);
+	}
+
+	//application
+	//new a document ... 
+	acDocManager->appContextNewDocument(LR"(@acadISO -Named Plot Styles.dwt)");
+	//appContextOpenDocument
+	//appContextRecoverDocument
+
+	{//do something on it
+		DocumentLock varDocumentLock;
+		auto varDataBase = acdbHostApplicationServices()->workingDatabase();
+		const wchar_t * varFileName;
+		varDataBase->getFilename(varFileName);
+		acutPrintf(LR"(CurrentDocument: %s
+)", varFileName);
+		//save the document
+		varDataBase->saveAs(LR"(D:/Project1/project.dwg)");
+	}
+
+	//application
+	acDocManager->closeDocument( acDocManager->curDocument() );
 
 }
 
