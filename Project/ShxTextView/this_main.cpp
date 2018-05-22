3c088aff5b009ace5b502b001e981047f3e1bc0d
nanguazhude <nanguazhude@vip.qq.com>
2018-05-22 14:47:41 +0800

    +Test Code

--- Project/ShxTextView/this_main.cpp
+++ Project/ShxTextView/this_main.cpp
@@ -12,16 +12,18 @@ using namespace std::string_view_literals;
 #include <vector>
 #include <forward_list>
 #include <string_view>
+#include <regex>
 
 using namespace std::string_view_literals;
 
 class Item {
 public:
 	QFileInfo file_info;
+	AcDbObjectId style_index;
 };
 
 /****************************************/
-AcDbMText * drawitem ( Item * i , int n  )   {
+AcDbMText * drawitem(Item * i, int n) {
 	AcDbMText * varItem = new AcDbMText;
 	std::wstring varData;
 	{
@@ -29,19 +31,79 @@ AcDbMText * drawitem ( Item * i , int n  )   {
 		varData += LR"b...e( 1沉孔2锪34%%p56%%d78×9今天0)b...e"sv;
 	}
 	varItem->setLocation({ 0, n * (-18.0) ,0 });
-	 
+	varItem->setTextStyle(i->style_index);
+	varItem->setWidth(1000);
+	varItem->setColumnWidth(1000);
 	varItem->setContents(varData.c_str());
-	
+
 	return varItem;
 }
 
-template<typename T,typename T1,typename B>
-void drawitems(T b,const T1 e,B d) {
+template<typename T, typename T1, typename B>
+void drawitems(T b, const T1 e, B d) {
 	int n = -1;
-	for (;b!=e;++b) {
-		auto v = drawitem(*b,++n);
+	for (; b != e; ++b) {
+		auto v = drawitem(*b, ++n);
 		d->appendAcDbEntity(v);
 		v->close();
+	}
+}
+
+void create_text_style(Item * i,int n){
+	auto varDB = acdbHostApplicationServices()->workingDatabase();
+	AcDbTextStyleTable* varTST = nullptr;
+	if(eOk!= varDB->getTextStyleTable(varTST,AcDb::kForWrite))
+		return;
+	std::wstring varStyleName{ LR"(text_style_name_)"sv };
+	{
+		auto varNumber = std::to_wstring(n);
+		switch (varNumber.size())
+		{
+		case 0:break;
+		case 1:varStyleName += LR"(00000)"sv; break;
+		case 2:varStyleName += LR"(0000)"sv; break;
+		case 3:varStyleName += LR"(000)"sv; break;
+		case 4:varStyleName += LR"(00)"sv; break;
+		case 5:varStyleName += LR"(0)"sv; break;
+		}
+		varStyleName += std::move(varNumber);
+	}
+	
+	AcDbTextStyleTableRecord * varR = nullptr ;
+	bool toInsert = false;
+	if (toInsert=(eOk == varTST->getAt(varStyleName.c_str(), i->style_index))) {
+		if (eOk != acdbOpenObject(varR, i->style_index, AcDb::kForWrite)) {
+			varTST->close(); 
+			return;
+		}
+	}
+
+	toInsert = !toInsert;
+
+	if (toInsert) { 
+		varR = new AcDbTextStyleTableRecord; 
+	}
+
+	varR->setName(varStyleName.c_str());
+	varR->setFileName(LR"(ztxt.shx)");
+	{
+		const auto varFN = i->file_info.absoluteFilePath().toStdWString();
+		varR->setBigFontFileName( varFN.c_str() );
+	}
+
+	if (toInsert) {
+		varTST->add(i->style_index, varR);
+	}
+
+	varR->close();
+	varTST->close();
+}
+
+template<typename T, typename T1 >
+void create_text_styles(T b, const T1 e) {
+	int n = -1;
+	for (; b != e; ++b) {
+		create_text_style(*b ,++n);
 	}
 }
 
@@ -54,21 +116,40 @@ inline static void show_shx_text() {
 	}
 	/**/
 	QDirIterator varIt{ varDirName,
-		QDir::NoDotAndDotDot | QDir::NoSymLinks | QDir::NoSymLinks,
+		QDir::NoDotAndDotDot | QDir::NoSymLinks | QDir::Files,
 		QDirIterator::NoIteratorFlags };
 
 	while (varIt.hasNext()) {
 		varIt.next();
 		const auto varFileInfo = varIt.fileInfo();
 		if (varFileInfo.suffix().toLower() == QStringLiteral("shx")) {
+
+			{/*检查当前文件是否是大字体文件*/
+				QFile varFile{ varFileInfo.absoluteFilePath() };
+				if (false == varFile.open(QIODevice::ReadOnly))
+					continue;
+				constexpr const static std::size_t size = 32;
+				char varTmp[size];
+				auto const varDataSize = varFile.read(varTmp, size);
+				if (varDataSize < size) { continue; }
+				const std::string_view view{ varTmp ,size };
+				const static std::regex varR(u8R"(bigfont)", std::regex::icase);
+				if (false == std::regex_search(view.begin(), view.end(), varR))
+					continue;
+			}
+
 			Item * i = &var_memory_list.emplace_front();
 			i->file_info = varFileInfo;
 			varItems.push_back(i);
+			if (varItems.size() > 1 ) { break; }
 		}
 	}
-
+	/*cretae text styles*/
+	{
+		create_text_styles(varItems.cbegin(), varItems.cend());
+	}
 	/*draw items*/
-	AcDbBlockTableRecord * pRec ;
+	AcDbBlockTableRecord * pRec;
 	{
 		const auto varDB = acdbHostApplicationServices()->workingDatabase();
 
@@ -87,6 +168,7 @@ inline static void show_shx_text() {
 	catch (...) {
 	}
 	pRec->close();
+
 }
 
 extern "C" AcRx::AppRetCode
--- project_all2018.pro
+++ project_all2018.pro
@@ -3,8 +3,9 @@ TEMPLATE = subdirs
 main_project.file =                 $$PWD/objectarx_2018.pro
 load_qt_library_project.file =      $$PWD/Project/LoadQtApplication/LoadQtDlls2018.pro
 set_mtext_background_project.file = $$PWD/Project/SetMTextBG/SBGABOF2018.pro
+shx_text_view.file                = $$PWD/Project/ShxTextView/ShxTextView2018.pro
 
 SUBDIRS += main_project
 SUBDIRS += load_qt_library_project
 SUBDIRS += set_mtext_background_project
-
+SUBDIRS += shx_text_view
