c1a762742e683c4804766d0da8717b9b3c2d986c
nanguazhude <nanguazhude@vip.qq.com>
2018-05-25 08:49:50 +0800

    +reformat

--- Project/Raw/MainWindow.cpp
+++ Project/Raw/MainWindow.cpp
@@ -2,48 +2,50 @@
 #include "ui_MainWindow.h"
 #include <QtCore/QtCore>
 
-class MainWindow::ThisPrivate{
+class MainWindow::ThisPrivate {
 public:
-    QString $FilePath;
-    QFile $File;
-    QString $FileData;
-    ThisPrivate(const QString & varFilePath ) : $FilePath(varFilePath) {
-        $File.setFileName($FilePath);
-        $File.open( QIODevice::ReadWrite );
-        QTextStream varStream{ &$File };
-        $FileData = varStream.readAll( );
-    }
-    void save(){
-        $File.reset();
-        const QByteArray varDataAboutToWrite = $FileData.toLocal8Bit();
-        $File.resize( varDataAboutToWrite.size()  );
-        $File.write( varDataAboutToWrite );
-        $File.close()/*final*/;
-    }
+	QString $FilePath;
+	QFile $File;
+	QString $FileData;
+	ThisPrivate(const QString & varFilePath) : $FilePath(varFilePath) {
+		$File.setFileName($FilePath);
+		$File.open(QIODevice::ReadWrite);
+		QTextStream varStream{ &$File }/*use qtextstream to decode*/;
+		$FileData = varStream.readAll();
+	}
+	void save() {
+		$File.reset();
+		const QByteArray varDataAboutToWrite = $FileData.toLocal8Bit();
+		$FileData.clear()/*final*/;
+		$File.resize(varDataAboutToWrite.size());
+		$File.write(varDataAboutToWrite);
+		$File.close()/*final*/;
+	}
 };
 
 MainWindow::MainWindow(const QString & varFilePath, QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow){
-    ui->setupUi(this);
-    thisp.reset( new ThisPrivate(varFilePath) ) ;
-    ui->textEdit->setPlainText( thisp->$FileData );
+	QMainWindow(parent),
+	ui(new Ui::MainWindow) {
+	ui->setupUi(this);
+	thisp.reset(new ThisPrivate(varFilePath));
+	ui->textEdit->setPlainText(thisp->$FileData)/*do not convert text format*/;
 }
 
-MainWindow::~MainWindow(){
-    delete ui;
+MainWindow::~MainWindow() {
+	delete ui;
 }
 
 void MainWindow::_p_save() try {
-    thisp->$FileData = ui->textEdit->toPlainText();
-    thisp->save();
-} catch( ... ){
-/*this function should not throw */
+	thisp->$FileData = ui->textEdit->toPlainText();
+	thisp->save();
+}
+catch (...) {
+	/*this function should not throw*/
 }
 
 void MainWindow::closeEvent(QCloseEvent *event) {
-    this->_p_save();
-    return Parent::closeEvent(event);
+	this->_p_save()/*save the changes when close*/;
+	return Parent::closeEvent(event);
 }
 
 
