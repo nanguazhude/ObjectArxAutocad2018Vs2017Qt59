b4bc117105f0952e3bba95388c123d602925fd78
nanguazhude <nanguazhude@vip.qq.com>
2018-05-25 08:55:41 +0800

    +Reset window size

--- Project/Raw/main.cpp
+++ Project/Raw/main.cpp
@@ -5,6 +5,7 @@ int main(int argc, char *argv[]){
 
     QApplication app(argc, argv);
     MainWindow window(QString::fromLocal8Bit(argv[1]));
+	window.resize(800,300);
     window.show();
 
     return app.exec();
