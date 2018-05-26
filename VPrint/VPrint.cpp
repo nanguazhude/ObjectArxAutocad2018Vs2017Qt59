af321138da9bc5864531a9cf1650fdb401b3d5b1
nanguazhude <nanguazhude@vip.qq.com>
2018-05-26 17:03:49 +0800

    +Test Code

--- VPrint/VPrint.cpp
+++ VPrint/VPrint.cpp
@@ -11,12 +11,14 @@ namespace {
 	public:
 		std::wstring $LayoutName;
 		AcDbDatabase *$DB;
+		AcDbLayerStateManager *$LM;
 	};
 }/*namespace*/
 
 ////////////////////////
 static inline void construct(ThisState * s) {
 	s->$DB = acdbHostApplicationServices()->workingDatabase();
+	s->$LM = s->$DB->getLayerStateManager();
 }
 
 ////////////////////////
@@ -35,24 +37,42 @@ static inline void getLayoutName(ThisState *s) {
 	}
 }
 
+//http://adndevblog.typepad.com/autocad/2012/05/listing-the-layout-names.html
 ////////////////////////
 static inline bool hasLayout(ThisState * s) {
 	//AcDbLayout
-	AcDbLayerStateManager * sm = s->$DB->getLayerStateManager();
-	return false;
+	//AcDbLayerStateManager * sm = s->$DB->getLayerStateManager();
+	auto & DB = s->$DB;
+	sstd::ArxClosePointer<AcDbDictionary > varLayouts;
+	if (eOk != DB->getLayoutDictionary(varLayouts.pointer(), kForRead))
+		svthrow(LR"(can not find layouts)"sv);
+	return varLayouts->has(s->$LayoutName.c_str());
+	/*std::unique_ptr<AcDbDictionaryIterator> varIt{ varLayouts->newIterator() };
+	if (nullptr == varIt) { svthrow(LR"(迭代失败)"sv); }
+	for (;!varIt->done();varIt->next()) {
+
+	}*/
+	//return false;
+}
+
+//http://adndevblog.typepad.com/autocad/2012/06/create-a-layout-using-objectarx.html
+////////////////////////
+static inline void createLayout(ThisState *s) {
+	auto LM = dynamic_cast<AcApLayoutManager *>(s->$LM);
+	if (LM == nullptr) { svthrow(LR"(can not find layout manager)"sv); }
+	AcDbObjectId layoutId;
+	AcDbObjectId blockTableRecId;
+	if(eOk!= LM->createLayout(s->$LayoutName.c_str(), layoutId, blockTableRecId))
+		svthrow(LR"(can not create layout)"sv);
+	//AcDbViewport
 }
 
 ////////////////////////
-static inline void createLayout(ThisState *) {
-
-}
-
-////////////////////////
-void sstd::VPrint::main() try{
+void sstd::VPrint::main() try {
 	auto varState = std::make_unique<ThisState>();
 	construct(varState.get());
 	getLayoutName(varState.get());
-	if (hasLayout(varState.get())) { 
+	if (hasLayout(varState.get())) {
 		/*如果已经有此name的layout,那么什么也不做...*/
 		return;
 	}
